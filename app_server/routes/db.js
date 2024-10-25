3253c80d0aafbc9a6b44f97de01220131e3ce1b7
laura looney <laura.looney@students.ittralee.ie>
2024-10-25 17:51:31 +0100

    MongoDB is not being my friend today

---
+++ app_server/routes/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const dbURI = "mongodb+srv://lollylooney:Gads1858@cluster0.2sjgu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+try {
+   
+mongoose.connect(
+    dbURI,
+    { useNewUrlParser: true, useUnifiedTopology: true }).then(
+    () => {console.log(" Mongoose is connected")},
+	err=> {console.log(err)}
+	);
+}
+ catch (e) {
+  console.log("could not connect");
+}//require('./locations');
+
